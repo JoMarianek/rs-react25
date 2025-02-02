1dbc6bcc41f5648ed251a0219f881a24ad55d648
Joelle <joelle.marianek@live.de>
2025-02-02 01:26:54 +0100

    feat: start setting up api call

--- src/CardList/CardList.tsx
+++ src/CardList/CardList.tsx
@@ -2,6 +2,19 @@ import { Component } from 'react';
 import styles from './CardList.module.css';
 
 class CardList extends Component {
+  // function fetchData() {
+  //   const url = "https://stapi.co/api/v2/rest/astronomicalObject/search";
+  //   try {
+  //     const response = await fetch(url);
+  //     if (!response.ok) {
+  //       throw new Error(`Response status: ${response.status}`)
+  //     }
+  //     const json = await response.json();
+  //   } catch (error) {
+  //       console.error(error.message);
+  //     }
+  // };
+
   render() {
     return (
       <div className={styles.cardList}>Results will be displayed here.</div>
