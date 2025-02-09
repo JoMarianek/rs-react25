0f3ee2b7a60f2f1749b46e5e13a9bf53eb30519d
Joelle <joelle.marianek@live.de>
2025-02-09 19:10:59 +0100

    refactor: app to only handle UI, add main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,11 @@
 import './styles/global.css';
 
-import { useState } from 'react';
-import { Outlet } from 'react-router-dom';
-
-import SearchComponent from './SearchComponent/SearchComponent';
-import CardList from './CardList/CardList';
-import Pagination from './Pagination/Pagination';
+import MainComponent from './MainComponent';
 
 function App() {
-  const [globalSearchTerm, setGlobalSearchTerm] = useState(
-    localStorage.getItem('starTrek_searchTerm') || ''
-  );
-
   return (
     <div>
-      <header>
-        <SearchComponent onSearch={setGlobalSearchTerm} />
-      </header>
-      <main>
-        <div className="mainContainer">
-          <CardList searchTerm={globalSearchTerm} />
-          <Outlet />
-        </div>
-        <Pagination />
-      </main>
+      <MainComponent />
       <footer>
         <button>Trigger ErrorBoundary</button>
       </footer>
