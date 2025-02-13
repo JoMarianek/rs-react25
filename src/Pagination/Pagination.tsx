dc92cd51cee163e1d69039d9345ba4a51b0ad04e
Joelle <joelle.marianek@live.de>
2025-02-13 18:00:19 +0100

    refactor: remove comments in pagination component

--- src/Pagination/Pagination.tsx
+++ src/Pagination/Pagination.tsx
@@ -3,9 +3,6 @@ import { Link } from 'react-router-dom';
 import styles from './Pagination.module.css';
 
 const Pagination = () => {
-  // const [searchParams] = useSearchParams();
-  // const currentPage = Number(searchParams.get('page')) || 0;
-
   const totalElements = 24;
   const itemsPerPage = 8;
   const totalPages = Math.ceil(totalElements / itemsPerPage);
