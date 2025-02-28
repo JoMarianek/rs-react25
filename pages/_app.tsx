40254f79ecfe584636ab9e92ac6731c244cc145c
Joelle <joelle.marianek@live.de>
2025-02-28 17:23:45 +0100

    feat: continue migration

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+
+import '../styles.global.css';
+import { store } from '../store';
+import { ThemeProvider } from '../contexts/ThemeProvider';
+import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary';
+import { StrictMode } from 'react';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <StrictMode>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <ThemeProvider>
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </Provider>
+      </ErrorBoundary>
+    </StrictMode>
+  );
+}
+
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
-import './styles/global.css';
-
 import MainComponent from './MainComponent';
 
 function App() {
