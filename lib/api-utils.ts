b811467f189c57e649fc4c0db786ef9148ee46d8
Joelle <joelle.marianek@live.de>
2025-03-04 17:07:25 +0100

    feat: create api-utils

---
+++ lib/api-utils.ts
@@ -0,0 +1,27 @@
+import { ITEMS_PER_PAGE } from '../config/config';
+
+const baseUrl = 'https://stapi.co/api/v2/rest/astronomicalObject'
+
+export const fetchAstronomicalObjects = async (page: number) => {
+  const url = `${baseUrl}/search?pageNumber=${page}&pageSize=${ITEMS_PER_PAGE}`;
+
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Error ${response.status}: ${response.statusText}`);
+  }
+
+  const json = await response.json();
+  return json.astronomicalObjects;
+};
+
+export const fetchSingleAstronomicalObject = async (uid: string) => {
+  const url = `${baseUrl}?uid=${uid}`;
+
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Error ${response.status}: ${response.statusText}`);
+  }
+
+  const json = await response.json();
+  return json.astronomicalObject;
+};
