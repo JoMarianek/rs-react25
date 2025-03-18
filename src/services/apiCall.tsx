9c4d4b80aed6e5e284cc1fb0884df425743bf6b9
Joelle <joelle.marianek@live.de>
2025-03-18 22:40:40 +0100

    chore: set up app

---
+++ src/services/apiCall.tsx
@@ -0,0 +1,14 @@
+const url = 'https://restcountries.com/v3.1/all?fields=name,capital,currencies';
+
+export const fetchCountries = async () => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Error fetching data: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
